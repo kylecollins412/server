e9fa3c7a65addaf2c508943ddee27efe1d74d302
Ayush Chugh <ayushchugh2006@gmail.com>
2022-08-09 23:55:14 +0530

    build(actions): :construction_worker: added deploy workflow

--- app.ts
+++ app.ts
@@ -86,9 +86,10 @@ cron.schedule("0 0 * * *", () => backupDB());
 
 /* --------------------------------- ANCHOR server --------------------------------- */
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.resolve("../", "client", "build")));
-	app.get("*", (req: Request, res: Response) => {
-		res.sendFile(path.resolve("../", "client", "build", "index.html"));
+	app.use(express.static(path.join(__dirname, "../client/build")));
+
+	app.get("/*", (req: Request, res: Response) => {
+		res.sendFile(path.join(__dirname, "../client/build/index.html"));
 	});
 }
 
